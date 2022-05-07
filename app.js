e806da6c712a6233102d23818db6a380c0db4b7b
Ines Herminio <84570798+inesherminio@users.noreply.github.com>
2022-05-07 10:04:57 +0100

    Lab reviewed

--- app.js
+++ app.js
@@ -1,79 +1,75 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
-const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
-
-
-  // Our routes go here:
-
-app.get('/', (req, res) => {
-  res.render('home')
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
-app.get('/artist-search', async (req, res) => {
+// Our routes go here:
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/artist-search", async (req, res) => {
+  /* With an eye on the future, where things get bigger and more complex, instead of using req.params or req.query inside the method,
+  destructure it ouside, like you do with artistsData (you're not passing data.body.artists.items to the view) */
+  const { artist } = req.query;
   try {
-    const data = await spotifyApi.searchArtists(req.query.artist);
+    const data = await spotifyApi.searchArtists(artist);
     const artistsData = data.body.artists.items;
-    res.render('artist-search-results', {artistsData})
-
+    res.render("artist-search-results", { artistsData });
+  } catch (err) {
+    console.error(err);
   }
-    catch (err) {
-      console.error(err)
-    }
-  
-})
+});
 
-app.get('/albums/:artistId', async (req, res)=> {
-  try{
-    const data = await spotifyApi.getArtistAlbums(req.params.artistId);
-    const albums = data.body.items;
-  res.render('albums', {albums})
-  }
-  catch (err) {
-    console.error(err)
-  }
-})
-
-app.get('/tracks/:tracksId', async (req, res) => {
+app.get("/albums/:artistId", async (req, res) => {
   try {
-    const data = await spotifyApi.getAlbumTracks(req.params.tracksId, { limit : 5, offset : 1 });
-    const tracks = data.body.items;
-    res.render('tracks', {tracks})
-  } 
-  catch(err) {
-    console.error(err)
+    const { artistId } = req.params;
+    const data = await spotifyApi.getArtistAlbums(artistId);
+    const albums = data.body.items;
+    res.render("albums", { albums });
+  } catch (err) {
+    console.error(err);
   }
-})
+});
 
+app.get("/tracks/:tracksId", async (req, res) => {
+  const { tracksId } = req.params;
+  try {
+    const data = await spotifyApi.getAlbumTracks(tracksId, {
+      limit: 5,
+      offset: 1,
+    });
+    const tracks = data.body.items;
+    res.render("tracks", { tracks });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-
-
-
-
-
-
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
-
-
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);

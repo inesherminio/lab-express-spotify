6641a6d6028fe95f8c90752f62ddeffd2a26b30e
Ines Herminio <84570798+inesherminio@users.noreply.github.com>
2022-05-07 09:50:07 +0100

    helena

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,67 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
+
+  // Our routes go here:
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+
+app.get('/artist-search', async (req, res) => {
+  try {
+    const data = await spotifyApi.searchArtists(req.query.artist);
+    const artistsData = data.body.artists.items;
+    res.render('artist-search-results', {artistsData})
+
+  }
+    catch (err) {
+      console.error(err)
+    }
+  
+})
+
+app.get('/albums/:artistId', async (req, res)=> {
+  try{
+    const data = await spotifyApi.getArtistAlbums(req.params.artistId);
+    const albums = data.body.items;
+  res.render('albums', {albums})
+  }
+  catch (err) {
+    console.error(err)
+  }
+})
+
+app.get('/tracks/:tracksId', async (req, res) => {
+  try {
+    const data = await spotifyApi.getAlbumTracks(req.params.tracksId, { limit : 5, offset : 1 });
+    const tracks = data.body.items;
+    res.render('tracks', {tracks})
+  } 
+  catch(err) {
+    console.error(err)
+  }
+})
+
+
+
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+
